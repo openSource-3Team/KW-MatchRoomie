12aabc0bc94e8bad18b768b8803ca14e88662836
hig1002 <hig7463@naver.com>
2024-12-01 08:04:25 +0900

    refactor : controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -5,27 +5,27 @@ export const postController = {
 		try {
 			const { title, content, authorId } = req.body;
 			const result = await postService.createPost(title, content, authorId);
-			res.status(201).json(result);
+			res.status(201).send(response(201, result));
 		} catch (err) {
-			res.status(400).json({ error: err.message });
+			res.status(400).send(response(400, { error: err.message }));
 		}
 	},
 
 	getAllPosts: async (req, res) => {
 		try {
 			const posts = await postService.getAllPosts();
-			res.status(200).json(posts);
+			res.status(200).send(response(200, posts));
 		} catch (err) {
-			res.status(500).json({ error: err.message });
+			res.status(500).send(response(500, { error: err.message }));
 		}
 	},
 
 	getPostById: async (req, res) => {
 		try {
 			const post = await postService.getPostById(Number(req.params.id));
-			res.status(200).json(post);
+			res.status(200).send(response(200, post));
 		} catch (err) {
-			res.status(404).json({ error: err.message });
+			res.status(404).send(response(404, { error: err.message }));
 		}
 	},
 
@@ -33,18 +33,18 @@ export const postController = {
 		try {
 			const { title, content } = req.body;
 			const result = await postService.updatePost(Number(req.params.id), title, content);
-			res.status(200).json(result);
+			res.status(200).send(response(200, result));
 		} catch (err) {
-			res.status(400).json({ error: err.message });
+			res.status(400).send(response(400, { error: err.message }));
 		}
 	},
 
 	deletePost: async (req, res) => {
 		try {
 			const result = await postService.deletePost(Number(req.params.id));
-			res.status(200).json(result);
+			res.status(200).send(response(200, result));
 		} catch (err) {
-			res.status(400).json({ error: err.message });
+			res.status(400).send(response(400, { error: err.message }));
 		}
 	},
 };
