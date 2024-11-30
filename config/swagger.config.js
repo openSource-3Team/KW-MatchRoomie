699ad58c121ec3af61282c1a1ae7a50b084490f5
hig1002 <129808885+hig1002@users.noreply.github.com>
2024-12-01 07:39:49 +0900

    Update swagger.config.js

--- config/swagger.config.js
+++ config/swagger.config.js
@@ -1,24 +1,19 @@
-import swaggerAutogen from "swagger-autogen";
+import SwaggerJsdoc from "swagger-jsdoc";
 
-const swaggerAutogenInstance = swaggerAutogen();
-
-const doc = {
-	info: {
-		title: "KW_MatchRoomie API",
-		version: "0.0.1",
-		description: "KW_MatchRoomie API with express, API 설명",
-	},
-	host: "15.165.223.198:3000", // 배포된 서버의 호스트와 포트
-	schemes: ["http"], // https를 사용하는 경우 ["https"]로 변경
-	basePath: "/",
-	servers: [
-		{
-			url: "http://15.165.223.198:3000", // EC2 퍼블릭 IP와 포트
+const options = {
+	definition: {
+		info: {
+			title: "KW_MatchRoomie API",
+			version: "0.0.1",
+			description: "KW_MatchRoomie API with express, API 설명",
 		},
-	],
+		servers: [
+	      {
+		url: "http://15.165.223.198:3000", // EC2 퍼블릭 IP와 포트
+	      },
+	    ],
+	},
+	apis: ["./src/routes/*.js", "./swagger/*"],
 };
 
-const outputFile = "./swagger-output.json"; // Swagger 문서가 생성될 위치
-const endpointsFiles = ["./src/routes/user.route.js"]; // 라우트 파일 경로를 지정
-
-swaggerAutogenInstance(outputFile, endpointsFiles);
+export const specs = SwaggerJsdoc(options);
